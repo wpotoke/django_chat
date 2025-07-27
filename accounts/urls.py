cf2ed0641df93ae25dda90c9576ea9074bad5253
moroz <morozovdaniel713@gmail.com>
2025-07-27 21:22:06 +0300

    chore: delete unused view and url - will be used elsewhere

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from accounts.views import UserRegisterView, index, UserLoginView
+from accounts.views import UserRegisterView, UserLoginView
 
 urlpatterns = [
-    path("home/", index, name="home"),
     path("register/", UserRegisterView.as_view(), name="user_register"),
     path("login/", UserLoginView.as_view(), name="user_login"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -12,15 +12,11 @@ from accounts.forms import UserCreationForm, UserLoginForm
 User = get_user_model()
 
 
-def index(request):
-    return render(request, "base.html")
-
-
 # Create your views here.
 class UserRegisterView(CreateView):
     form_class = UserCreationForm
     template_name = "accounts/user_register.html"
-    success_url = reverse_lazy("user_login")
+    success_url = reverse_lazy("home")
 
     def get_context_data(self, **kwargs) -> dict[str, Any]:
         context = super().get_context_data(**kwargs)
