7b26d320b94e0192a80af98fff6462de52a78a7f
moroz <morozovdaniel713@gmail.com>
2025-07-30 20:35:09 +0300

    feat(user): extend user profile functionality
    - Added Profile model with related fields:
      - First/last name, avatar, bio, birthday
      - Slug field for profile URLs
      - Image validation for avatars
    - Enhanced User model:
      - Added verbose names for admin interface
      - Improved phone number field description
    - Implemented additional utilities:
      - Automatic slug generation using unique_slugify
      - get_absolute_url for profile pages
      - File extension validation for avatars
    - Added Meta options for better admin display

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,9 @@
-from django.core.validators import RegexValidator
+from django.core.validators import RegexValidator, FileExtensionValidator
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.urls import reverse
 from accounts.manager import MyUserManager
+from services.utils import unique_slugify
 
 
 class User(AbstractBaseUser, PermissionsMixin):
@@ -18,6 +20,7 @@ class User(AbstractBaseUser, PermissionsMixin):
         error_messages={
             "unique": "This username is already taken.",
         },
+        verbose_name="Имя пользователя",
     )
     email = models.EmailField(max_length=255, verbose_name="Email", unique=True)
     is_active = models.BooleanField(default=True)
@@ -28,7 +31,9 @@ class User(AbstractBaseUser, PermissionsMixin):
         regex=r"^((\+7)|8)\d{10}$",
         message="Phone number must be entered in the format: '+79999999999' or '89999999999'.",
     )
-    phone_number = models.CharField(validators=[phone_regex], max_length=12, null=True, blank=True, unique=True)
+    phone_number = models.CharField(
+        validators=[phone_regex], max_length=12, null=True, blank=True, unique=True, verbose_name="Номер телефона"
+    )
     objects = MyUserManager()
 
     USERNAME_FIELD = "email"
@@ -46,3 +51,35 @@ class User(AbstractBaseUser, PermissionsMixin):
     @property
     def is_admin(self) -> bool:
         return self.is_staff or self.is_superuser
+
+
+class Profile(models.Model):
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE, verbose_name="Пользователь")
+    slug = models.SlugField(max_length=255, blank=True, verbose_name="URL", unique=True)
+    first_name = models.CharField(max_length=200, verbose_name="Имя")
+    last_name = models.CharField(max_length=255, verbose_name="Фамилия")
+    avatar = models.ImageField(
+        verbose_name="Аватар",
+        upload_to="images/avatars/%Y/%m/%d",
+        default="images/avatars/default.png",
+        blank=True,
+        validators=[FileExtensionValidator(allowed_extensions=("png", "jpg", "jpeg"))],
+    )
+    bio = models.TextField(max_length=192, blank=True, verbose_name="Инфа о себе")
+    birthday = models.DateField(null=True, blank=True, verbose_name="Дата рождения")
+
+    class Meta:
+        ordering = ("user",)
+        verbose_name = "Профиль"
+        verbose_name_plural = "Профили"
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = unique_slugify(self, self.user.username, self.slug)
+        super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse("profile_detail", kwargs={"slug": self.slug})
