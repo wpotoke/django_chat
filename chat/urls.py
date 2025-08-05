63b41424634d0c7aa1924228ae7b8edbc0c3bb2d
moroz <morozovdaniel713@gmail.com>
2025-08-05 20:25:45 +0300

    prepare url for view

--- chat/urls.py
+++ chat/urls.py
@@ -3,5 +3,7 @@ from chat import views
 
 urlpatterns = [
     path("", views.home_view, name="home"),
+    path("chats/", views.chat_list, name="chat_list"),
     path("groups/<uuid:uuid>/", views.group_chat_view, name="group"),
+    # path("chats/<uuid:uuid>/", ) view not
 ]
